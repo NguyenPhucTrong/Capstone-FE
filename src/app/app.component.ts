325ef7e007a382569659c9a98432aad9f654aa99
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-12-04 16:50:56 +0700

    silder-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatBotPagesComponent } from "./pages/chat-bot-pages/chat-bot-pages.component";
 import { DisplayType } from './shared/enums/display-type.enum';
+import { SilderbarComponent } from "./pages/silderbar/silderbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatBotPagesComponent],
+  imports: [RouterOutlet, ChatBotPagesComponent, SilderbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/silderbar/silderbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-silderbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './silderbar.component.html',
+  styleUrl: './silderbar.component.css'
+})
+export class SilderbarComponent {
+
+}
