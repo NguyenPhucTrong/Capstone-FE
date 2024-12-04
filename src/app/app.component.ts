7dcc74905ded660c73c0637ed93997fdd9ffdb56
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-12-04 23:45:09 +0700

    sidebar đã ổn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { ChatBotPagesComponent } from "./pages/chat-bot-pages/chat-bot-pages.component";
 import { DisplayType } from './shared/enums/display-type.enum';
 import { SidebarComponent } from "./pages/silderbar/silderbar.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatBotPagesComponent, SidebarComponent],
+  imports: [RouterOutlet, ChatBotPagesComponent, SidebarComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Capstone-FE';
-  public displayType = DisplayType;
+  isSidebarOpen: boolean = true;
 
+  toggleSidebar() {
+
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
